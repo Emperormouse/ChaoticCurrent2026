d989c3a035cbf26cc5872f0e8278a9ffb0d9e8de
Emperormouse <148268601+Emperormouse@users.noreply.github.com>
2025-11-19 22:43:01 +0000

    stuff
    stuffstuffstuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/Tests/AprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/Tests/AprilTag.java
@@ -1,123 +1,36 @@
-package org.firstinspires.ftc.teamcode.code.Tests;
+package org.firstinspires.ftc.teamcode.code.TeleOp;
 
-import static java.lang.Math.toRadians;
-
-import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.teamcode.code.Subsystems.Bot;
+import org.firstinspires.ftc.teamcode.code.utility.Op;
+import org.firstinspires.ftc.teamcode.code.utility.Side;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Disabled
-@Autonomous
+@TeleOp
 public class AprilTag extends LinearOpMode {
-    enum Pattern {
-        GPP,
-        PGP,
-        PPG
-    }
     public void runOpMode() {
-        AprilTagProcessor aprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
-        VisionPortal visionPortal = VisionPortal.easyCreateWithDefaults(
-            hardwareMap.get(WebcamName.class, "camera"),
-            aprilTagProcessor
-        );
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        Bot bot = new Bot(hardwareMap, drive.localizer, Side.BLUE, telemetry);
+        bot.initialize();
 
-        Pattern pattern = Pattern.GPP;
-        while(!opModeIsActive()) {
-            ArrayList<AprilTagDetection> aprilTags = aprilTagProcessor.getDetections();
+        waitForStart();
 
-            switch(aprilTags.get(aprilTags.size()-1).id) {
-                case 21: pattern = Pattern.GPP; break;
-                case 22: pattern = Pattern.PGP; break;
-                case 23: pattern = Pattern.PPG; break;
-            }
-
-            telemetry.addData("Number: ", aprilTags.get(aprilTags.size()-1).id);
-            telemetry.addData("Pattern: ", pattern);
-            telemetry.addData("Size: ", aprilTags.size());
+        while(opModeIsActive()) {
+            bot.aprilTagTelementary();
+            bot.updatePoseUsingAprilTag();
             telemetry.update();
-        }
 
-        Pose2d startPos = new Pose2d(61, -20, toRadians(180));
-        Vector2d launchVec = new Vector2d(55, -15);
-        double launchHeading = toRadians(200);
-        Pose2d launchPose = new Pose2d(launchVec.x, launchVec.y, launchHeading);
-        MecanumDrive drive = new MecanumDrive(hardwareMap, startPos);
+            double r = bot.turnBasedOnAprilTag(Side.BLUE) - gamepad1.right_stick_x;
 
-        Action pgp = drive.actionBuilder(startPos)
-            .waitSeconds(2)
-            .strafeToLinearHeading(launchVec, launchHeading)
-            .waitSeconds(3)
-            .setTangent(toRadians(180))
-            .splineToLinearHeading(new Pose2d(35, -50, toRadians(-90)), toRadians(-90))
-            .strafeToLinearHeading(launchVec, launchHeading)
-            .waitSeconds(3)
+            double y = -gamepad1.left_stick_y;
+            double x = -gamepad1.left_stick_x * 1.1;
+            y /= Math.max(1, Math.abs(r*2));
+            x /= Math.max(1, Math.abs(r*2));
 
-            .splineToLinearHeading(new Pose2d(12, -50, toRadians(-90)), toRadians(-90))
-            .strafeToLinearHeading(new Vector2d(0, -60), toRadians(180))
-            .waitSeconds(3)
-            .strafeToLinearHeading(launchVec, launchHeading)
-            .waitSeconds(3)
-            .build();
-
-        Action gpp = drive.actionBuilder(startPos)
-            .waitSeconds(2)
-            .strafeToLinearHeading(launchVec, launchHeading)
-            .waitSeconds(4)
-            .setTangent(toRadians(180))
-            .splineToLinearHeading(new Pose2d(12, -40, toRadians(-90)), toRadians(-90))
-            .lineToY(-50)
-            .strafeToLinearHeading(launchVec, launchHeading)
-            .waitSeconds(3)
-
-            .splineToLinearHeading(new Pose2d(36, -40, toRadians(-90)), toRadians(-90))
-            .lineToY(-50)
-            .strafeToLinearHeading(new Vector2d(0, -60), toRadians(180))
-            .waitSeconds(3)
-            .strafeToLinearHeading(launchVec, launchHeading)
-            .waitSeconds(3)
-            .build();
-
-        Action ppg = drive.actionBuilder(startPos)
-            .waitSeconds(2)
-            .strafeToLinearHeading(launchVec, launchHeading)
-            .waitSeconds(4)
-
-            .setTangent(toRadians(180))
-            .splineToSplineHeading(new Pose2d(12, -50, toRadians(-90)), toRadians(-90))
-            .setTangent(toRadians(90))
-            .waitSeconds(0.5)
-            .splineToLinearHeading(launchPose, 0)
-            .waitSeconds(4)
-
-            .splineToLinearHeading(new Pose2d(7, -54, 0), toRadians(-90))
-            .waitSeconds(2)
-            .lineToX(23)
-            .setTangent(0)
-            .splineToSplineHeading(new Pose2d(33, -40, toRadians(100)), toRadians(90))
-            .setTangent(toRadians(90))
-            .splineToSplineHeading(launchPose, toRadians(45))
-            .waitSeconds(4)
-            .lineToX(launchVec.x+1)
-            .build();
-
-        switch (pattern) {
-            case GPP: Actions.runBlocking(gpp); break;
-            case PGP: Actions.runBlocking(pgp); break;
-            case PPG: Actions.runBlocking(ppg); break;
+            bot.moveFieldCentric(x, y, r, Op.TELE);
         }
     }
 }
